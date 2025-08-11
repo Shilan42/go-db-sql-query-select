a779ddc214ca45de9f179bd1e84fa60a8fd6b634
Шиленков Андрей <a.v.shilenkov@yandex.ru>
2025-08-11 18:43:38 +0700

    Реализовал функцию selectSales для выборки всех продаж по конркетному клиенту

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 
 	_ "modernc.org/sqlite"
 )
 
+// Sale представляет собой структуру, описывающую продажу товара
 type Sale struct {
 	Product int
 	Volume  int
@@ -18,23 +20,55 @@ func (s Sale) String() string {
 	return fmt.Sprintf("Product: %d Volume: %d Date:%s", s.Product, s.Volume, s.Date)
 }
 
+// selectSales выполняет выборку всех продаж для указанного клиента
 func selectSales(client int) ([]Sale, error) {
+	// Открываем подключение к базе данных
+	db, err := sql.Open("sqlite", "demo.db")
+	if err != nil {
+		fmt.Println("Ошибка при подключении к БД:", err)
+		return nil, err
+	}
+	// Закрываем подключение после завершения работы
+	defer db.Close()
+
+	// Слайс для хранения результатов
 	var sales []Sale
 
-	// напишите код здесь
+	// Выполняем SQL-запрос с параметрами product, volume, date из таблицы sales
+	rows, err := db.Query("SELECT product, volume, date FROM sales WHERE client = :client", sql.Named("client", client))
+	if err != nil {
+		fmt.Println("Ошибка при выполнении запроса:", err)
+		return nil, err
+	}
+	// Закрываем результат запроса
+	defer rows.Close()
+
+	// Парсим результаты запроса и записываем в структуру sale
+	for rows.Next() {
+		sale := Sale{}
+		err := rows.Scan(&sale.Product, &sale.Volume, &sale.Date)
+		if err != nil {
+			fmt.Println("Ошибка при сканировании строки:", err)
+			return nil, err
+		}
+		// Добавляем результат в слайс
+		sales = append(sales, sale)
+	}
 
 	return sales, nil
 }
 
 func main() {
+	// ID клиента для выборки
 	client := 208
 
+	// Получаем продажи для указанного клиента
 	sales, err := selectSales(client)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
+	// Выводим результаты
 	for _, sale := range sales {
 		fmt.Println(sale)
 	}
